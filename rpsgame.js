9159f84a25c565e2f30f730709d60ee4dec0c92e
Rohit james <Rohitjames77@outlook.com>
2023-09-21 19:52:03 +0530

    add gameplay function

--- rpsgame.js
+++ rpsgame.js
@@ -1,13 +1,18 @@
+let greet = "Hi There ! Wanna Play some ROCK PAPER SCISSORS ?"
+console.log(greet);
+
 let choices = ["rock", "paper","scissors"];
 function getComputerChoice(){
 console.log(choices[(Math.floor(Math.random()*choices.length))]);
 }
-getComputerChoice()
-getComputerChoice()
-getComputerChoice()
-getComputerChoice()
-getComputerChoice()
-getComputerChoice()
 
+  let win = "You Win!"
+  let lose = "You lose"
+  let tie = "That a Tie"
+  function gameplay(playerSelection,computerSelection){
+    if(playerSelection == choices[1] && computerSelection == getComputerChoice()){
+     console.log(lose);   
 
-
+    }
+}
+    gameplay(1,2)
