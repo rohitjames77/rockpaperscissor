bcd436a1ad3ac9052898b0c68eb00201efcb3624
Rohit james <Rohitjames77@outlook.com>
2023-09-21 13:50:41 +0530

    add computer random output function

---
+++ rpsgame.js
@@ -0,0 +1,13 @@
+let choices = ["rock", "paper","scissors"];
+function getComputerChoice(){
+console.log(choices[(Math.floor(Math.random()*choices.length))]);
+}
+getComputerChoice()
+getComputerChoice()
+getComputerChoice()
+getComputerChoice()
+getComputerChoice()
+getComputerChoice()
+
+
+
