33f51eafdd6677f9928d176eb2e18684ee1f5ae0
Rohit james <Rohitjames77@outlook.com>
2023-09-26 13:23:05 +0530

    add loop for round

--- rpsgame.js
+++ rpsgame.js
@@ -1,18 +1,33 @@
 let greet = "Hi There ! Wanna Play some ROCK PAPER SCISSORS ?"
-console.log(greet);
 
-let choices = ["rock", "paper","scissors"];
+let input = ["rock", "paper","scissors"];
 function getComputerChoice(){
-console.log(choices[(Math.floor(Math.random()*choices.length))]);
+  console.log(input[(Math.floor(Math.random()*input.length))]);
+}
+ 
+let playerSelection= "rock";
+let computerSelection = getComputerChoice();
+function playRound(playerSelection, computerSelection ){   
+if (playerSelection == "rock" && computerSelection == getComputerChoice() ){
+  true;
+}
 }
 
-  let win = "You Win!"
-  let lose = "You lose"
-  let tie = "That a Tie"
-  function gameplay(playerSelection,computerSelection){
-    if(playerSelection == choices[1] && computerSelection == getComputerChoice()){
-     console.log(lose);   
 
-    }
-}
-    gameplay(1,2)
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+  
+
+ 
+
