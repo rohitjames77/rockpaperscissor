ab875402db1d8bb06f07b0a1edc88718509a2382
rohit james <rohitjames77@outlook.com>
2023-09-27 10:14:53 +0530

    add comparison statement

--- rpsgame.js
+++ rpsgame.js
@@ -1,15 +1,51 @@
-let intro = prompt("hi Wanna Play Rock Paper Scissor", "Your Choice")
+
+
+let input = prompt("Rock Paper Scissors");
+let playerSelection = input;
 let choices = ["rock", "paper","scissors"];
 function getComputerChoice(){
 return(choices[(Math.floor(Math.random()*choices.length))]);
 }
-
-let playerSelection = "rock";
 let computerSelection = getComputerChoice();
-function playRound(computerSelection,playerSelection){
-   if (computerSelection == "paper" && playerSelection == "rock" )
-console.log( "You lose ! Paper beats Rock");
+
+function playRound(playerSelection,computerSelection){
+   var victory= "you win";
+   var loss = "You lose";
+   var tied = "Its a Tie";
+if (playerSelection == computerSelection){
+return tied;
+}else if  (playerSelection == "paper" && computerSelection == "rock"){
+   return victory;
+} else if ( playerSelection == "rock" &&  computerSelection == "paper"){
+   return loss;
+} else if (playerSelection == "scissor" && computerSelection == "paper"){
+   return victory;
+}else if (playerSelection == "paper" && computerSelection == "scissors"){
+   return loss;
+}else if ( playerSelection == "rock" && computerSelection == " scissors"){
+   return victory; 
+}else if ( playerSelection == "scissors" && computerSelection == "rock"){
+   return loss;
 }
+}
+
+function game(playRound){
+for(var i = 0; i< 5; i++){
+   return(playRound(playerSelection, computerSelection));
+}
+}
+console.log(game(playRound));
+console.log(game(playRound));
+console.log(game(playRound));
+
+
+
+
+ 
+
+
+
+
 
 
 
