1d750d5181064204102d53171c6e1a6ffc471235
Rohit james <Rohitjames77@outlook.com>
2023-09-27 18:22:16 +0530

    add if else logic

--- rpsgame.js
+++ rpsgame.js
@@ -1,52 +1,30 @@
-
-
-let input = prompt("Rock Paper Scissors");
-let playerSelection = input;
-let choices = ["rock", "paper","scissors"];
-function getComputerChoice(){
-return(choices[(Math.floor(Math.random()*choices.length))]);
-}
-let computerSelection = getComputerChoice();
-
-function playRound(playerSelection,computerSelection){
-   var victory= "you win";
-   var loss = "You lose";
-   var tied = "Its a Tie";
-if (playerSelection == computerSelection){
-return tied;
-}else if  (playerSelection == "paper" && computerSelection == "rock"){
-   return victory;
-} else if ( playerSelection == "rock" &&  computerSelection == "paper"){
-   return loss;
-} else if (playerSelection == "scissor" && computerSelection == "paper"){
-   return victory;
-}else if (playerSelection == "paper" && computerSelection == "scissors"){
-   return loss;
-}else if ( playerSelection == "rock" && computerSelection == " scissors"){
-   return victory; 
-}else if ( playerSelection == "scissors" && computerSelection == "rock"){
-   return loss;
-}
-}
-
-function game(playRound){
-for(var i = 0; i< 5; i++){
-   return(playRound(playerSelection, computerSelection));
-}
-}
-console.log(game(playRound));
-console.log(game(playRound));
-console.log(game(playRound));
-
-
-
-
  
 
-
-
-
-
-
-
-
+ prompt("Rock Paper Scissors", );
+//console.log("hi "+ playerselection);
+let choices = ["rock", "paper","scissors"];
+ function getComputerChoice(){
+return(choices[(Math.floor(Math.random()*choices.length))]);
+}
+let playerSelection = "rock";
+let computerSelection = getComputerChoice();
+function playRound(playerSelection,computerSelection){
+   if ( playerSelection === "paper" && computerSelection === "rock" || 
+        playerSelection === "rock" && computerSelection === "scissors" ||
+        playerSelection === "scissors"&& computerSelection ==="paper"
+   ){
+  console.log("win");
+   }
+   else if (playerSelection === "rock" && computerSelection === "paper" ||
+             playerSelection === "paper" && computerSelection === "scissors" ||
+             playerSelection === "scissors" && computerSelection === "rock"
+   ){
+console.log("lose");
+   }else if(playerSelection == computerSelection ){
+console.log("tie");
+}
+ 
+}
+playRound();
+playRound();
+playRound();
