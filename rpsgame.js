1c814e92c2bc1326ce60def9d58d448c0ec57915
rohit james <rohitjames77@outlook.com>
2023-09-29 20:46:03 +0530

    add result condtions

--- rpsgame.js
+++ rpsgame.js
@@ -5,13 +5,14 @@
 
 function getComputerChoice() {
 let computerSelection = input[Math.floor(Math.random() * input.length)];
-
+console.log(computerSelection);
 return computerSelection;
 }
 
 
 function playerInput(){
    let playerSelection= prompt("Whats ur Choice , ROCK PAPER SCISSORS?",);
+   console.log(playerSelection);
    return playerSelection;
 }
 
@@ -19,13 +20,41 @@ function playerInput(){
 
 function playRound(playerSelection,computerSelection){
 if ( playerSelection == "rock" && computerSelection == "paper"){
-   return "You Lose";
-} else {
-   return "you Win";
+
+   return "computer";
+}if(playerSelection == "paper" && computerSelection == "rock"){
+return "player";
+}
+if (playerSelection == "scissors" && computerSelection == "rock"){
+return "computer";
+}if (playerSelection == "rock" && computerSelection == "scissors"){
+return "player";
+}
+if (playerSelection == "paper" && computerSelection == "scissors"){
+return "computer";
+}
+if (playerSelection == "scissors" && computerSelection == "paper"){
+return 
+}else {
+   return "tied";
 }
 
 }
-let computerSelection= getComputerChoice();
-let playerSelection = playerInput();
+
+let playerSelection= playerInput();
+computerSelection= getComputerChoice(); 
 console.log(playRound(playerSelection,computerSelection));
-   
+
+function game(){
+   let result = score(playerSelection,computerSelection)
+      if(result == "player"){
+      return "Player Wins";
+   } 
+   else if (result == "computer"){
+      return "Computer Wins";
+   }else { return " Its tied";
+
+   }
+   }
+
+game();
