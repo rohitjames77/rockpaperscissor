5c576e231311c583c4be5aebcd74ee0cc238f926
rohit james <rohitjames77@outlook.com>
2023-10-02 20:18:30 +0530

    fix loop

--- rpsgame.js
+++ rpsgame.js
@@ -1,11 +1,9 @@
- 
 
  let input = ["rock","paper","scissors"];
 
-
 function getComputerChoice() {
 let computerSelection = input[Math.floor(Math.random() * input.length)];
-console.log(computerSelection);
+//console.log(computerSelection);
 return computerSelection;
 }
 
@@ -13,48 +11,39 @@ return computerSelection;
 function playerInput(){
    let playerSelection= prompt("Whats ur Choice , ROCK PAPER SCISSORS?",);
    console.log(playerSelection);
-   return playerSelection;
+return playerSelection;
 }
-
-
-
-function playRound(playerSelection,computerSelection){
-if ( playerSelection == "rock" && computerSelection == "paper"){
-
-   return "computer";
-}if(playerSelection == "paper" && computerSelection == "rock"){
-return "player";
-}
-if (playerSelection == "scissors" && computerSelection == "rock"){
-return "computer";
-}if (playerSelection == "rock" && computerSelection == "scissors"){
-return "player";
-}
-if (playerSelection == "paper" && computerSelection == "scissors"){
-return "computer";
-}
-if (playerSelection == "scissors" && computerSelection == "paper"){
-return 
-}else {
-   return "tied";
-}
-
-}
-
 let playerSelection= playerInput();
 computerSelection= getComputerChoice(); 
-console.log(playRound(playerSelection,computerSelection));
+//console.log(playRound(playerSelection,computerSelection));
 
-function game(){
-   let result = score(playerSelection,computerSelection)
-      if(result == "player"){
-      return "Player Wins";
-   } 
-   else if (result == "computer"){
-      return "Computer Wins";
-   }else { return " Its tied";
+   
+function playRound(playerSelection,computerSelection){
+if ( playerSelection == "rock" && computerSelection == "paper"){
+return `You Win ${playerSelection} beats ${computerSelection} `;
+}if(playerSelection == "paper" && computerSelection == "rock"){
+return `You Win! ${playerSelection} beats ${computerSelection}`;
+}if (playerSelection == "scissors" && computerSelection == "rock"){
+return `You Lose! ${computerSelection} beats ${playerSelection}`;
+}if (playerSelection == "rock" && computerSelection == "scissors"){
+return `You Win! ${playerSelection} beats ${computerSelection}`;
+}if (playerSelection == "paper" && computerSelection == "scissors"){
+return `You Lose! ${computerSelection} beats ${playerSelection}`;
+}if (playerSelection == "scissors" && computerSelection == "paper"){
+return`You Win! ${playerSelection} beats ${computerSelection}`;
+}else {
+return `${playerSelection} & ${computerSelection} Its a tie `;
+}
 
-   }
-   }
+}
+
+
+function game (){
+for (let i = 0 ;i < 5;i++) {
+   let playerSelection= playerInput();
+   computerSelection= getComputerChoice(); 
+   console.log(playRound(playerSelection,computerSelection));
+}
+}
 
 game();
